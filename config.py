204287b3601112164ad918a55680cf4adafe4f1a
Dude2dude <105360180+Dude2dude@users.noreply.github.com>
2025-05-01 13:57:54 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+API_ID = "22182189"
+API_HASH = "5e7c4088f8e23d0ab61e29ae11960bf5'"
+BOT_TOKEN = "1931784190:AAHYFh9G-xRJwgYwpNDAE-ndp2lkPH5LucQ"
+
+FILE_CHANNEL = -1002613710001  # Channel ID (not username)
+FORCE_SUB_CHANNEL = "@moviecenter224"  # Channel users must join
+
+MONGO_URI = "mongodb+srv://godplan:godplan@cluster0.5i4ua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
